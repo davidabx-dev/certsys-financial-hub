7562323d0db169242165e42ef0d13a6b5980d3f7
davidabx-dev <dvd1.0js@gmail.com>
2026-03-01 12:22:35 -0300

    feat: adiciona k8s deployment, config JSF 2.x e engenharia reversa MUMPS

---
+++ legacy_system/transferencia.m
@@ -0,0 +1,11 @@
+﻿; Rotina MUMPS Legada - CACHÉ 2010.2
+; Modulo de Transferencia Financeira a ser migrado para Java
+Transfer(contaOrigem, contaDestino, valor)
+    new saldoAtual
+    set saldoAtual = ^BancoDados("Contas", contaOrigem, "Saldo")
+    if saldoAtual < valor do  quit "SALDO INSUFICIENTE"
+    . write "Erro de saldo",!
+    ; Realiza a transação CMT legada
+    set ^BancoDados("Contas", contaOrigem, "Saldo") = saldoAtual - valor
+    set ^BancoDados("Contas", contaDestino, "Saldo") = ^BancoDados("Contas", contaDestino, "Saldo") + valor
+    quit "TRANSFERENCIA APROVADA"
